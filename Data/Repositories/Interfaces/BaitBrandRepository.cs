470ab081a3ab8e0814826ce61705a58c076bc63d
Longji Kang <kanglongjidev@gmail.com>
2025-05-19 14:10:40 +0200

    Add bait brand repository

---
+++ Data/Repositories/Interfaces/BaitBrandRepository.cs
@@ -0,0 +1,58 @@
+﻿using Fishing_API.Data.DBContexts;
+using Fishing_API.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Fishing_API.Data.Repositories.Interfaces {
+    public class BaitBrandRepository : IFishingRepository<BaitBrandModel> {
+        private readonly DatabaseContext _databaseContext;
+
+        public BaitBrandRepository(DatabaseContext databaseContext) {
+            _databaseContext = databaseContext;
+        }
+
+        public async Task<BaitBrandModel?> Add(BaitBrandModel entity) {
+            if (Find(entity) == null) {
+                BaitBrandModel dbEntry = (await _databaseContext.BaitBrands.AddAsync(entity)).Entity;
+                await _databaseContext.SaveChangesAsync();
+
+                return dbEntry;
+            } else {
+                return null;
+            }
+        }
+
+        public async Task<BaitBrandModel?> Remove(BaitBrandModel entity) {
+            BaitBrandModel? dbEntry = await Find(entity);
+
+            if (dbEntry != null) {
+                _databaseContext.BaitBrands.Remove(dbEntry);
+                await _databaseContext.SaveChangesAsync();
+
+                return dbEntry;
+            } else {
+                return null;
+            }
+        }
+
+        public async Task<BaitBrandModel?> Update(BaitBrandModel entity) {
+            BaitBrandModel? dbEntry = await Find(entity);
+
+            if (dbEntry != null) {
+                _databaseContext.BaitBrands.Update(entity);
+                await _databaseContext.SaveChangesAsync();
+
+                return dbEntry;
+            } else {
+                return null;
+            }
+        }
+
+        public async Task<BaitBrandModel?> Find(BaitBrandModel entity, bool includeNestedObjects = false) {
+            BaitBrandModel? baitBrand = await _databaseContext.BaitBrands
+                .Where(b => b.Brand == entity.Brand)
+                .FirstOrDefaultAsync();
+
+            return baitBrand;
+        }
+    }
+}
--- Data/Repositories/Interfaces/IFishingRepository.cs
+++ Data/Repositories/Interfaces/IFishingRepository.cs
@@ -2,6 +2,9 @@
 
 namespace Fishing_API.Data.Repositories.Interfaces {
     public interface IFishingRepository<T> {
-        
+        public Task<T?> Add(T entity);
+        public Task<T?> Remove(T entity);
+        public Task<T?> Update(T entity);
+        public Task<T?> Find(T entity, bool includeNestedObjects = false);
     }
 }
